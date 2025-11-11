54bff46ec0487a1b3235f472319b56888ce166bf
Alice Dar Bello <alicedarbello@hotmail.com>
2025-11-11 19:45:57 -0300

    Feat: Add Loaging page to routes

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function HomePageLoading() {
+  return (
+    <div className="flex h-full w-full items-center justify-center py-20">
+      <div className="h-16 w-16 animate-spin rounded-full border-b-2 border-gray-900" />
+    </div>
+  );
+}
